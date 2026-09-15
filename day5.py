6d04ea3ec4c0e887a4ebf02610b809f2e6177731
Bilal Noor Muhammad Memon <bilalnoormuhammad12@gmail.com>
2026-09-15 06:16:12 -0700

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,11 @@
+#Escape Sequences  \n 
+print("Hey I am a good boy \nand this veiwer is also a good boy/girl")
+#Double Qutaion mai "Bilal"  \"\"
+print("Assalamualikum Myself \"BILAL\" ")
+#Single Qutation Hi' myself bilal \'
+print("Hi\' myself bilal")
+#join Hey ~7~6   sep="~"
+print("Hey", 7,6,sep="~~" )
+#end="123"
+print("Bilal noor ",end="123")
+print("King")
---
+++ day6.py
@@ -0,0 +1,20 @@
+#Variable AND DataType
+a=1
+print(a)
+b="Bilal"
+print(b)
+#Type 
+c=2
+d="NOOR"
+e= True
+f=complex(8,2)
+print("The Type of c is",type(c))
+print("The Type of d is",type(d))
+print("The Type of e is",type(e))
+print("The Type of f is",type(f))
+#2+1i
+g=complex(2,5)
+print(g)
+#Disnari
+ditc1={"Name":"Bilal","Age": 17, "CanVote": "True", "Behaviyar": "Nice"}
+print(ditc1)
