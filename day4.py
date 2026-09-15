b9f6c6c80f9b51c36de04285f2d32ebb19438dca
Bilal Noor Muhammad Memon <bilalnoormuhammad12@gmail.com>
2026-09-15 05:19:29 -0700

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,9 @@
+print("Hello World", 7, "Bilal")
+print(5)
+print('Bye')
+print(5*5)
+
+print("Twinkle, twinkle, little star,")
+print("""How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.""")
