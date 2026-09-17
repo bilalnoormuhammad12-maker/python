5bd8f8352990ad46cb3bbd107df526ff9e544883
Bilal Noor Muhammad Memon <bilalnoormuhammad12@gmail.com>
2026-09-16 18:22:41 -0700

    Add files via upload

---
+++ calculation7.py
@@ -0,0 +1,26 @@
+print(2+5)
+print(2-5)
+print(2*5)
+print(2/2)
+#Division Operaters
+print(15//2)
+#Model
+print(5%2)
+#Expotension
+print(5**3)
+
+#Quiz Create a calculator capable of performing addition, subtraction, multiplication and division operations on two numbers. Your program should format the output in a readable manner!
+a=2
+a1=2
+print(a+a1)
+print(a-a1)
+print(a*a1)
+print(a/a1)
+#Eik or mathod hai quiz ko hal karne ka
+
+b=2
+b1=2
+print("The value of", b,"+",b1,"is: ",b+ b1)
+print('The value of',b, "-",b1,"is:",b-b1 )
+print("The value of", b,"*",b1,"is: ",b* b1)
+print("The value of", b,"/",b1,"is: ",b/ b1)
