6bf78c058b51f9e4e59ac35c75e14fab2398daa6
Bilal Noor Muhammad Memon <bilalnoormuhammad12@gmail.com>
2026-09-22 23:22:19 +0500

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,23 @@
+#String
+name="Bilal"
+print('My name is:',name)
+print("My "+ name)
+# b=('My Name is:\"BILAL\" mY FATHER nAME IS:\"Noor Muhammad\"')
+b='Hello \"BILAL\"'
+print(b)
+#### Mutiple Lines
+c= """"Hi BILAL
+This is good Boy
+Ve are intellegent,\""""
+print(c)
+# Index
+names="Bilal"
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+# Foor Loop
+print('Foor Loop')
+for  range in name:
+    print(range)
