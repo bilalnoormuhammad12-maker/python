5859119862c5ed27c780a5d42e2b22e20c26de70
Bilal Noor Muhammad Memon <bilalnoormuhammad12@gmail.com>
2026-09-22 23:46:27 +0500

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,22 @@
+# String Slicing
+a= "Bilal, Haris"
+print(a[0:5])
+# Len Function
+print(len(a))
+# 2 Example
+b="NOOR MUHAMMAD"
+c=len(b)
+print(c)
+# Slicing
+fruit="Mango"
+print(fruit[0:4])
+print(fruit[:4])
+print(fruit[1:4])
+print(fruit[:])
+print(fruit[:5])
+print(fruit[:-3])
+print(fruit[-3:-1])
+print(fruit[-2:-1])
+
+
+                                   
---
+++ day12test.py
@@ -0,0 +1,6 @@
+a="Harry"
+print(a[-4:-2])
+b="Bilal"
+print(b[-2:-1])
+c="Haris"
+print(c[-4:-3])
